4d5414e601b5b40c370ee483d94891fdb732682b
gitze <georg@itzenplitz.ch>
2020-12-27 18:58:46 +0100

    Update setAppleIDPassword.sh

--- daemon/setAppleIDPassword.sh
+++ daemon/setAppleIDPassword.sh
@@ -1,16 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # using read command without any variable
 # change to "read -sp" if password input should be hidden
 
 configparam="$1"
-
-[[ "$configparam" == "" ]] && echo "please pass locstion of xapsd.yaml as first paramater." 
-for configfile in  "$configfile" '/etc/xapsd.yaml' '/usr/local/etc/xapsd/xapsd.yaml' ;
+for configfile in  "$configparam" "/etc/xapsd.yaml" "/usr/local/etc/xapsd/xapsd.yaml" ;
 do
     [[ "$configfile" == ""  ]] && continue
     [[ -f "$configfile" ]] && break
 done
-[[ ! -f "$configfile" ]] && echo "ConfigFile '$configparam' not found. Exiting script" && exit 1
+if [[ ! -f "$configfile" ]]
+then
+    echo && echo "ERROR:"
+    [[ "$configparam" == "" ]] && echo "Please pass location of xapsd.yaml as first paramater."
+    echo "ConfigFile '$configparam' not found. Exiting script"
+    exit 1
+fi
 
 echo "Using config file: ./$configfile"
 
@@ -20,8 +24,8 @@ read -p 'Password: ' passvar
 [[ "$passvar" == "" ]] && echo "No password set. Exiting script" && exit 1
 hashvalue=`echo -n "$passvar" | openssl dgst -sha256 -binary | xxd -p -c 32`
 
-sed -E -i .bak "s/^(appleId[[:blank:]]*[=|:][[:blank:]]*).*/\1$uservar/" "$configfile"
-sed -E -i .bak "s/^(appleIdHashedPassword[[:blank:]]*[=|:][[:blank:]]*).*/\1$hashvalue/" "$configfile"
+sed -E -i "" "s/^(appleId[[:blank:]]*[=|:][[:blank:]]*).*/\1$uservar/" "$configfile"
+sed -E -i "" "s/^(appleIdHashedPassword[[:blank:]]*[=|:][[:blank:]]*).*/\1$hashvalue/" "$configfile"
 
 echo
 echo "Seting new parameter in $configfile"
